66ce667b73e97ddd167eff199d92f2eaf42c96d9
BhanuPrasadYeligeti <bhanuprasadyeligeti@gmail.com>
2025-03-19 20:04:17 +0530

    updated

---
+++ src/Primepair.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Primepair {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the n value");
+        n=sc.nextInt();
+        ArrayList<Integer> arr=new ArrayList<>();
+        int k=0;
+        for(int i=2;i<n;i++)
+
+        {
+            int count=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    count+=1;
+                }
+            }
+            if(count<=2)
+            {
+                arr.add(i);
+
+            }
+        }
+        int c=0;
+        for(int i=0;i<arr.size();i++)
+        {
+
+            for(int j=i;j<arr.size();j++)
+            {
+                if(arr.get(i)+arr.get(j)==n)
+                {
+                    System.out.println(arr.get(i)+"+" +arr.get(j)+"->"+n);
+                    c+=1;
+                }
+
+            }
+
+        }
+        if(c==0)
+        {
+            System.out.print("the given value doesn't contains values");
+        }
+
+    }
+}
---
+++ src/Stringcount.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Stringcount {
+    public static void main(String[] args) {
+        String str =" ";
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the string");
+        str=sc.nextLine();
+
+        String[] arr = str.split(" ");
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].equals(arr[j])) {
+                    count++;
+                    arr[j] = "";
+                }
+            }
+
+            if (count > 1 && !arr[i].equals("")) {
+                System.out.println(arr[i] + ": " + count);
+            }
+        }
+    }
+
+}
